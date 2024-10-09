92ea79e7bf13c7bc4ce582d2d54360b00bf50834
IsfandiyorOlimovvv <shoxagaming@gmail.com>
2024-10-09 22:38:46 +0500

    js first homework

---
+++ app.js
@@ -0,0 +1,68 @@
+
+2
+
+// let names = prompt ("what is your name");
+// console.log(names);
+// let age = prompt ("how old are you");
+// console.log(age);
+// let city = prompt ("what is your city");
+// console.log(city);
+// let address = prompt ("what is your address");
+// console.log(address);
+// let phonenumber = prompt ("what is your phone number");
+// console.log(phonenumber);
+// let course = prompt ("what is your course");
+// console.log(course);
+// let coursenumber = prompt ("what is your course number");
+// console.log(coursenumber);
+// let subject = prompt ("what is your favourite subject");
+// console.log(subject);
+// let familyMembers = prompt ("how many your family members");
+// console.log(familyMembers);
+// let hobbies = prompt ("what is your hobbie");
+// console.log(hobbies);
+
+1
+
+// let a = prompt("Birinchi sonni kiriting:");
+// let b = prompt("Ikkinchi sonni kiriting:");
+// let operator = prompt("Operatorni tanlang (+, -, *, /):");
+
+// let result = eval(a + operator + b);
+
+// console.log("Natija: " + result);
+// alert("Natija: " + result);
+
+4
+
+// let math = prompt("Matemdan baxoingizni kiriting");
+// let physics = prompt("Fizikadan baxoingizni kiriting");
+// let gym = prompt("Fizradan baxoizni kiriting");
+// let english = prompt("Ingiz tilidan baxoizni kiriting");
+// let historyy = prompt("Tarixdan baxoizni kiriting");
+
+// math = Number(math);
+// physics = Number(physics);
+// gym = Number(gym);
+// english = Number(english);
+// historyy = Number(historyy);
+
+// let total = math + physics + gym + english + historyy;
+
+// let average = total / 5;
+
+// console.log("O'rtacha baho: " + average);
+// alert("O'rtacha baho: " + average);
+
+3
+
+// let olma = Number(prompt("Olmani narxni kiriting:"));
+// let bexi = Number(prompt("Bexini narxni kiriting:"));
+// let banan = Number(prompt("Bananni narxni kiriting:"));
+// let olcha = Number(prompt("Olchani narxini kiriting:"));
+// let uzum = Number(prompt("Uzumni narxini kiriting:"));
+
+// let total = olma + bexi + banan + olcha + uzum;
+
+// console.log("Umumiy narx: " + total);
+// alert("Umumiy narx: " + total);
